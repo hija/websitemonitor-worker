6a3cfdab45b7c06d82f9e2b98feabee23f3d6ba0
hija <hilko.j@wwu.de>
2020-08-01 23:40:09 +0200

    Updated to flake8

--- monitor/monitor.py
+++ monitor/monitor.py
@@ -1,13 +1,28 @@
 import requests
 
+
 class WebsiteMonitor:
-
-
+    """
+    Monitors a website
+    """
 
     @staticmethod
-    def check_website_status(website: str, follow_redirects=True):
+    def check_website_status(website: str, follow_redirects=True) -> int:
+        """Return the status code of a website.
+
+        Args:
+            website (str): Websiteurl as string.
+            follow_redirects (bool, optional):
+                Whether redirects should be followed or not. Defaults to True.
+
+        Returns:
+            Int: HTTP-Statuscode, -1 bei Fehler
+        """
         try:
-            response = requests.get(website, timeout=1, allow_redirects=follow_redirects)
+            response = requests.get(
+                website, timeout=1, allow_redirects=follow_redirects)
             return int(response.status_code)
-        except:
-            return None
+        except requests.HTTPError:
+            return -1
+        except requests.Timeout:
+            return -1
--- monitor/test_monitor.py
+++ monitor/test_monitor.py
@@ -4,23 +4,36 @@ import unittest
 
 
 class TestWebsiteMonitor(TestCase):
+    """
+    Tests all of WebsiteMonitor methods
+    """
+
     def test_check_website_status(self):
-        ## TC1: Website is online ; returns a statuscode
-        self.assertEqual(200, WebsiteMonitor.check_website_status('https://hilko.eu'))
-        self.assertEqual(500, WebsiteMonitor.check_website_status('http://httpstat.us/500'))
-        
+        """
+        Tests if check_website_status works
+        Different test cases are explained in code.
+        """
+        # TC1: Website is online ; returns a statuscode
+        self.assertEqual(
+            200, WebsiteMonitor.check_website_status('https://hilko.eu'))
+        self.assertEqual(
+            500, WebsiteMonitor.check_website_status('http://httpstat.us/500'))
 
-        ## TC2: Website is online ; redirects
-        self.assertEqual(200, WebsiteMonitor.check_website_status('http://httpstat.us/301'))
-        self.assertEqual(301, WebsiteMonitor.check_website_status('http://httpstat.us/301', follow_redirects=False))
+        # TC2: Website is online ; redirects
+        self.assertEqual(
+            200, WebsiteMonitor.check_website_status('http://httpstat.us/301'))
+        self.assertEqual(301, WebsiteMonitor.check_website_status(
+            'http://httpstat.us/301', follow_redirects=False))
 
+        # TC3: Server is not reachable (i.e. no DNS entry or server offline)
+        self.assertEqual(-1,
+                         WebsiteMonitor.check_website_status(
+                             'https://xaxaxaxaxa.hilko.eu'))
 
-        ## TC3: Server is not reachable (i.e. no DNS entry or server offline)
-        self.assertEqual(None, WebsiteMonitor.check_website_status('https://xaxaxaxaxa.hilko.eu'))
+        # TC4: Counter-Test
+        self.assertNotEqual(
+            200, WebsiteMonitor.check_website_status('http://httpstat.us/404'))
 
-        ## TC4: Counter-Test
-        self.assertNotEqual(200, WebsiteMonitor.check_website_status('http://httpstat.us/404'))
-        
 
 if __name__ == '__main__':
     unittest.main()
